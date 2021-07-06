519ea3e92e65d3a6b2983d30b4f97be813aaea95
Piotr Ostrouch <piotr.ostrouch@gmail.com>
2021-07-07 00:02:34 +0200

    updates

---
+++ App/Controllers/Signup.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controllers;
+
+use \Core\View;
+
+class Signup extends \Core\Controller {
+	
+	public function newAction(){
+		
+		View::renderTemplate('Signup\new.html');
+	}
+	
+	public function createAction(){
+		var_dump($_POST);
+		
+	}
+	
+}
---
+++ App/Models/User.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use PDO;
+
+class User extends \Core\Model {
+	
+	public function save(){
+		
+		$
+		$sql = 'INSERT INTO users VALUE (:username, :password, :email)';
+	}
+}
