7cb6a6a0403f4bbcca85d7f839cf94231abdc6ec
Piotr Ostrouch <piotr.ostrouch@gmail.com>
2021-07-07 14:14:41 +0200

    validation form in php

--- App/Controllers/Signup.php
+++ App/Controllers/Signup.php
@@ -4,6 +4,10 @@ namespace App\Controllers;
 
 use \Core\View;
 
+use \App\Models\User;
+
+
+
 class Signup extends \Core\Controller {
 	
 	public function newAction(){
@@ -12,7 +16,23 @@ class Signup extends \Core\Controller {
 	}
 	
 	public function createAction(){
-		var_dump($_POST);
+		//var_dump($_POST);
+		$user = new User($_POST);
+		$user->validate();
+		
+		if(empty($user->errors))
+		{
+		
+			$user->save();
+			//$user->success = "New user Has been Added";
+			//View::renderTemplate('login\new.html', ['user'=>$user]);
+			header('Location: http://'.$_SERVER['HTTP_HOST'].'/BudgetMVC/public/?login/new');
+		
+		}else{
+			//var_dump($user->errors);
+			View::renderTemplate('Signup\new.html', ['user'=>$user]);
+			$user->errors = [];
+		}
 		
 	}
 	
--- App/Models/User.php
+++ App/Models/User.php
@@ -6,9 +6,86 @@ use PDO;
 
 class User extends \Core\Model {
 	
-	public function save(){
+	public $errors = [];
+	public $success = [];
+	
+	public function __construct($data=[]){
 		
-		$
-		$sql = 'INSERT INTO users VALUE (:username, :password, :email)';
+		foreach($data as $key=>$value){
+			$this->$key=$value;
+		}
 	}
+	
+	public  function save(){
+		
+		$password_hash = password_hash($this->pass, PASSWORD_DEFAULT);
+		$sql = 'INSERT INTO users VALUE (NULL,:username, :password, :email)';
+		$db = static::getDB();
+		$stmt = $db->prepare($sql);
+		$stmt->bindValue(':username', $this->login, PDO::PARAM_STR);
+		$stmt->bindValue(':password', $password_hash, PDO::PARAM_STR);
+		$stmt->bindValue(':email', $this->email, PDO::PARAM_STR);
+		return $stmt->execute();
+	}
+	
+	public function validate(){
+		
+		if($this->login==''){
+			$this->errors[]="Name must be required";
+		}
+		
+		if(strlen($this->login)<3){
+			$this->errors[] = "login Must must consist of at least 3 characters";
+		}
+		
+		if($this->pass != $this->confirmPass){
+			
+			$this->errors[]="Passwords do not match";
+		}
+		
+		if(strlen($this->pass)<3){
+			$this->errors[] = "Password must be longer than 3 characters";
+		}
+		
+		if(preg_match('/[a-z]+/i',$this->pass)==0){
+			$this->errors[] = "Password must contain at least one letter";
+		}
+		
+		if(preg_match('/\d+/', $this->pass)==0){
+			$this->errors[]="Password must containt at least one number";
+		}
+		
+		if(filter_var($this->email, FILTER_VALIDATE_EMAIL)===false)
+		{
+			$this->errors[] = "Email is not valid";
+		}
+		
+		if(! $this->emailExist($this->email)){
+			$this->errors[]='Email already exist';
+		}
+	}
+	
+	public static function findByEmail($email){
+		
+		$sql = "SELECT * FROM users WHERE email = :email";
+		
+		$db = static::getDB();
+		$stmt = $db->prepare($sql);
+		
+		$stmt->bindValue(':email', $email, PDO::PARAM_STR);
+		$stmt->setFetchMode(PDO::FETCH_CLASS, get_called_class());
+		$stmt->execute();
+		return $stmt->fetch();
+	}
+	
+	public function emailExist($email){
+		
+		if(static::findByEmail($email)){
+			return false;
+		}
+		return true;
+	}
+	
+	
+	
 }
